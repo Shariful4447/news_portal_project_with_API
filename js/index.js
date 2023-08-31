b14abaa3b9297ba1b293c7404d3d5d28cd96c56b
Shariful4447 <shariful.islam4447@gmail.com>
2023-08-31 04:48:41 +0200

    add category in tab

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
-const handleData = () => {
-    fetch('https://openapi.programming-hero.com/api/news/categories')
-    .then(res => res.json())
-    .then(data =>console.log(data))
-    .catch((err)=>console.log(err));
+// for category name
+const handleCategory = async() => {
+    
+    const response =await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await response.json();
+    // get tab container by id
+    const tabcontainer = document.getElementById('tab-container');
+
+    data.data.news_category.slice(0,3).forEach((category) => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <a class="tab">${category.category_name}</a> 
+        `;
+        tabcontainer.appendChild(div);
+    })
+    console.log(data.data.news_category);
+
 }
 
-handleData();
+handleCategory();
